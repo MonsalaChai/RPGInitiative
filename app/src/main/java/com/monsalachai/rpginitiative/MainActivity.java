c0ff788f2e3c360e87c0221faffbcb86fbf6936f
Mesalu <samuel@mesalu.com>
2019-04-06 14:15:26 -0700

    Use a drawerlayout as the base layout, add the drawer toggle, and drawer  listener (even  though its deprecated?)

--- app/src/main/java/com/monsalachai/rpginitiative/MainActivity.java
+++ app/src/main/java/com/monsalachai/rpginitiative/MainActivity.java
@@ -1,6 +1,9 @@
 package com.monsalachai.rpginitiative;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -15,6 +18,17 @@ public class MainActivity extends AppCompatActivity implements CharacterInfoFrag
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+
 
         ArrayList<DummyContent.DummyItem> items = DummyContent.ITEMS;
         if (savedInstanceState == null) {
