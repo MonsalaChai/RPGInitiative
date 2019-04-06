eca6cfd019c96d06529cfb343aeacb7faa03fdba
Milan Justel <milancjustel@gmail.com>
2019-04-06 13:52:33 -0700

    Added CharacterInfo fragment and list
    Added fragment to show the list of characters and initiatives. This is
    using most of the default stuff added by the list fragment.

--- app/src/main/java/com/monsalachai/rpginitiative/MainActivity.java
+++ app/src/main/java/com/monsalachai/rpginitiative/MainActivity.java
@@ -2,19 +2,31 @@ package com.monsalachai.rpginitiative;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.monsalachai.rpginitiative.dummy.DummyContent;
 import com.monsalachai.rpginitiative.ui.main.MainFragment;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements CharacterInfoFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        ArrayList<DummyContent.DummyItem> items = DummyContent.ITEMS;
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
+                    //.replace(R.id.container, MainFragment.newInstance())
+                    .replace(R.id.container, CharacterInfoFragment.newInstance(1, items))
                     .commitNow();
         }
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+        Log.i("fragment", "item " + item);
     }
 }
